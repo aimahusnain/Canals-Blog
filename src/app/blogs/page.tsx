188d63a8649924e7cbd4f9581174d4e85916d209
aimahusnain <aimahusnain@gmail.com>
2024-01-14 03:00:27 +0500

    hn

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,19 +1,19 @@
 import BlogList from "@/components/blogs/blog-list";
 
 async function extractAllBlogs() {
-    const res = await fetch(`${process.env.URL}/api/blog-post/get-all-post`, {
-      method: "GET",
-      cache: "no-store",
-    });
-  
-    const data = await res.json();
-  
-    if (data.success) return data.data;
-  }
-  
-  export default async function Blogs(){
 
-    const blogPostsList = await extractAllBlogs();
+  const res = await fetch(`${process.env.URL}/api/blog-post/get-all-post`, {
+    method: "GET",
+    cache: "no-store",
+  });
 
-    return( <BlogList lists={blogPostsList}/> )
-  }
+  const data = await res.json();
+
+  if (data.success) return data.data;
+}
+
+export default async function Blogs() {
+  const blogPostsList = await extractAllBlogs();
+
+  return <BlogList lists={blogPostsList} />;
+}
